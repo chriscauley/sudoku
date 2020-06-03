f06dae40f6a1b323c9582aba545a1ea2d1e94aad
chriscauley <chris@lablackey.com>
2020-06-03 21:32:22 +0000

    youtube link on main page, dont link puzzles without ctc data

--- client/game/Index.js
+++ client/game/Index.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import css from '@unrest/css'
 import RestHook from '@unrest/react-rest-hook'
 
 const withPuzzles = RestHook('/api/puzzle/')
+
+const PuzzleLink = (props) => {
+  const { external_id, video_id, title } = props
+  return (
+    <div className="mb-2">
+      {external_id ? (
+        <Link to={`/puzzle/ctc/${external_id}/`} className={css.link()}>
+          {title} #{external_id}
+        </Link>
+      ) : (
+        <span>{title} (no puzzle)</span>
+      )}
+      <a
+        href={`https://www.youtube.com/watch?v=${video_id}`}
+        className={css.icon('youtube mx-2')}
+      />
+    </div>
+  )
+}
 
 const Index = (props) => {
   const { puzzles = [] } = props.api
@@ -15,9 +35,7 @@ const Index = (props) => {
         </li>
         {puzzles.map((puzzle) => (
           <li key={puzzle.id}>
-            <Link to={`/puzzle/ctc/${puzzle.external_id}/`}>
-              {puzzle.title} ({puzzle.external_id})
-            </Link>
+            <PuzzleLink {...puzzle} />
           </li>
         ))}
       </ul>
