a87ef9afd7c3e65be6107c17bb0cc5e4074e318d
chriscauley <chris@lablackey.com>
2020-06-17 12:22:11 +0000

    client now defaults to using https://sudoku.unrest.io as the default domain, local developent must specify SUDOKU_URL="" as an env variable

--- client/app.js
+++ client/app.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter, Route } from 'react-router-dom'
-import { alert } from '@unrest/core'
+import { alert, config as ur_config } from '@unrest/core'
+import { settings as rest_settings } from '@unrest/react-rest-hook'
 
 import config from './config'
 import Nav from './components/Nav'
 import game from './game'
 import classnames from 'classnames'
+
+const { SUDOKU_URL = 'https://sudoku.unrest.io' } = process.env
+rest_settings.root_url = ur_config.base_url = SUDOKU_URL
 
 const App = config.connect((props) => {
   document.body.className = classnames(props.config.formData)
